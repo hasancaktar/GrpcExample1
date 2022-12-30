6dc22025eacac0813696bf0f30797f0f5c59de75
iar545 <iar545@IAR545>
2022-12-30 17:34:19 +0300

    Add project files.

---
+++ GrpcService/Services/MessageService.cs
@@ -0,0 +1,54 @@
+﻿using Grpc.Core;
+using MessageService;
+
+namespace GrpcService.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        //Server Streaming
+        //public override async Task SendMessage(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        //{
+        //    Console.WriteLine($"isim: {request.Name} | Mesaj: {request.Message}");
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        await Task.Delay(1000);
+        //        await responseStream.WriteAsync(new MessageResponse { Message = "Merhaba " + i });
+        //    }
+        //}
+
+
+        //Client Streamin
+        //public override async Task<MessageResponse> SendMessage(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+        //{
+        //    while (await requestStream.MoveNext(context.CancellationToken))
+        //    {
+        //        Console.WriteLine($"isim: {requestStream.Current.Name} | Mesaj: {requestStream.Current.Message}");
+        //    }
+
+        //    return new MessageResponse
+        //    {
+        //        Message = "Veri alındı.."
+        //    };
+        //}
+
+
+        //bi-directional streaming
+        public override async Task SendMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            var task1= Task.Run(async () =>
+            {
+                while (await requestStream.MoveNext(context.CancellationToken))
+                {
+                    Console.WriteLine($"isim: {requestStream.Current.Name} | Mesaj: {requestStream.Current.Message}");
+                }
+            });
+            for (int i = 0; i < 10; i++)
+            {
+                 await Task.Delay(1000);
+                 await responseStream.WriteAsync(new MessageResponse { Message = "Mesaj"+i });
+            }
+
+            await task1;
+        }
+    }
+}
---
+++ MyClient/Program.cs
@@ -0,0 +1,75 @@
+﻿using Grpc.Net.Client;
+using MessageService;
+namespace MyClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5265");
+            var messageClient = new Message.MessageClient(channel);
+            //----unary yöntemi
+            //MessageResponse response = messageClient.SayHello(new MessageRequest { Message = "Gönderilen Mesaj", Name = "Hasan" });
+
+
+            //---server streaming
+            //var response = messageClient.SendMessage(new MessageRequest
+            //{
+            //    Message = "Stream yöntemi",
+            //    Name = "Stream"
+            //});
+
+            //----CLIENT STREAMING
+            //await  response.ResponseAsync(new MessageRequest { Message = "MESAJ", Name = "İSİM" });
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            //while (await response.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            //{
+
+            //    Console.WriteLine(response.ResponseStream.Current.Message);
+            //}
+
+
+            //----CLIENT STREAMING
+            //var request = messageClient.SendMessage();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    await Task.Delay(1000);
+            //    await request.RequestStream.WriteAsync(new MessageRequest { Message = "Mesaj " + i, Name = "İsim" });
+            //}
+
+            ////stream  datanın sonlandığını ifade eder
+            //await request.RequestStream.CompleteAsync();
+
+            //Console.WriteLine((await request.ResponseAsync).Message);
+
+
+
+            var request = messageClient.SendMessage();
+
+            //request işlemini farklı bir thread da yapıyoruz burada
+            var task1 = Task.Run(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    await request.RequestStream.WriteAsync(new MessageRequest
+                    {
+                        Message = "Mesaj " + i,
+                        Name = "İsim"
+                    });
+                }
+            });
+            while (await request.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            {
+                Console.WriteLine(request.ResponseStream.Current.Message);
+               
+
+                 
+            }
+            await task1;
+            await request.RequestStream.CompleteAsync();
+            
+
+        }
+    }
+}
